7f31e8e8ac838343f07558131b307c1653eb991f
dimam <dima.masis@checkmarx.com>
2017-02-12 18:42:26 +0200

    demo _python robot framework

---
+++ login_tests/MyLibrary.py
@@ -0,0 +1,13 @@
+# from robot.libraries.BuiltIn import BuiltIn
+# from selenium.webdriver import Remote
+# import logging
+#
+#
+# def get_current_browser():
+#      library_instance = BuiltIn().get_library_instance('Selenium2Library')
+#
+#
+#     return browser
+#
+#
+# def
